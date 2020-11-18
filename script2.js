ec65c1243af7d26ab0dd1c117d5d03643d32f140
Razdank <50101691+Razdank@users.noreply.github.com>
2020-11-18 14:13:43 +0300

    1
    1

---
+++ script2.js
@@ -0,0 +1,7 @@
+if (process.argv[2]>0){
+	console.log(process.argv[3]);
+} else if (process.argv[2]<0){
+	console.log(process.argv[3].split('').reverse().join(''));
+} else {
+	console.log("undefined behavior");
+}
